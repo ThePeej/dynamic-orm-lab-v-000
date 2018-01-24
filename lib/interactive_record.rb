d2e7865fa1ee5b81fcba4a84890f5fe50a1ceae0
PJ Frias <peejfrias@gmail.com>
2018-01-24 17:47:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -31,12 +31,10 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    column_names = self.class.column_names.delete_if{|column|
+    column_names = self.class.column_names
+    column_names.delete_if{|column| 
       binding.pry
-      column = "id"
-    }
-    binding.pry
-    column_names
+      column = "id"}
   end
 
 
