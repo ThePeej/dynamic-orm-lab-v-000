a9bc111abaaf5615969490885753d65dea514df6
PJ Frias <peejfrias@gmail.com>
2018-01-24 17:37:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,7 +14,6 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
 
-    binding.pry
     table_info.each do |column|
       column_names << column["name"]
     end
